cde8f646ca8ae3c8551044b8362e0027775fb518
salesvictor <salesrvictor@gmail.com>
2018-06-30 21:59:39 -0300

    functional server

---
+++ app/static/js/chat.js
@@ -0,0 +1,33 @@
+var url = 'http://' + document.domain + ':' + location.port
+var socket = io.connect(url)
+
+socket.on('connect', connected)
+socket.on('my response', response)
+
+function connected(){
+  socket.emit('my event', {data: 'User Connected'})
+
+  var form = $('form').on('submit', submitted)
+}
+
+function submitted(e){
+  e.preventDefault()
+  
+  let user_name = $('input.username').val()
+  let user_input = $('input.message').val()
+
+  socket.emit('my event', {user_name: user_name, message: user_input})
+
+  $('input.message').val('').focus()
+}
+
+function response(msg){
+  console.log(msg)
+
+  if (typeof msg.user_name !== 'undefined'){
+    $('h3').remove()
+
+    let text_box = '<div><b style="color: #000">' + msg.user_name + '</b>: ' + msg.message + '</div>'
+    $('div.message_holder').append(text_box)
+  }
+}
